6f134257141a21fc31917bb24db507f860b5be4d
Soniya Taneja <soniyataneja23@gmail.com>
2024-07-02 21:13:05 +0530

    updated background image

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
 
-let countEl = document.getElementById("count-el")
-let previousClicks = localStorage.getItem         ('clicks')?parseInt(localStorage.getItem('clicks')):0;
+let countEl = document.getElementById("count-el");
+let previousClicks = localStorage.getItem;         ('clicks')?parseInt(localStorage.getItem('clicks')):0;
 countEl.textContent = previousClicks;
 let count = previousClicks;
 function increment()
 {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
     localStorage.setItem('clicks',count);
 }
 
 
-let saveEl = document.getElementById("save-el")
-let saved = localStorage.getItem('savedEntries');
+let saveEl = document.getElementById("save-el");
+let saved = localStorage.getItem('savedEntries') ?localStorage.getItem('savedEntries'):"";
+
 saveEl.textContent = saved;
 let countStr = saved;
 
 function save()
 {
 
-    countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = count
-    localStorage.setItem('savedEntries',saveEl.textContent)
+    countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = count;
+    localStorage.setItem('savedEntries',saveEl.textContent);
 
 }
 
-// ?localStorage.getItem('savedEntries'):""
